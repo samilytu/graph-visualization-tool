ebb606c8697ada061c4bb6dd287e66701900f2fe
Bedrettin Şamil ÖZTÜRK <bedrettin.ozturk@tubitak.gov.tr>
2023-05-11 20:57:00 +0300

    İlk commit

--- src/components/GraphCanvas/GraphCanvas.js
+++ src/components/GraphCanvas/GraphCanvas.js
@@ -14,12 +14,12 @@ const GraphCanvas = () => {
 
     return (
         <div className="graph-canvas" onClick={drawNode}>
-            <svg  style={{ width: '100%', height: '100%' }} className="svgg">
+            <svg style={{ width: '100%', height: '100%' }} className="svgg">
                 {nodes.map((node, index) => (
                     <g key={index}>
-                        <circle cx={node.x} cy={node.y} r="15" fill="black" />
+                        <circle cx={node.x} cy={node.y} r="15" ill="black" />
                         <text
-                        
+
                             x={node.x}
                             y={node.y}
                             cursor={'default'}
@@ -30,8 +30,8 @@ const GraphCanvas = () => {
                             fill="white"
 
                             fontSize="14px"
-                            
-                            
+
+
                         >
                             {index + 1}
                         </text>
